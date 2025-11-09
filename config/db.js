dacf6c4f625dafd862883fbc7ddd956d2e16ee14
Layek Miah <layekofficial63@gmail.com>
2025-11-09 10:55:35 +0600

    modify db connect function

--- config/db.js
+++ config/db.js
@@ -1,25 +1,11 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGODB_URI;
-let cached = global.mongoose;
-
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
 const connectDB = async () => {
-  if (cached.conn) {
-    // Reuse existing connection
-    return cached.conn;
-  }
+  mongoose.connection.on("connected", () => {
+    console.log("connected to database");
+  });
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connectDB;
