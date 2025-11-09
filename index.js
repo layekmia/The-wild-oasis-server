3331ee63fb932626f98968c3b77e4a3b75bf2ed3
Layek Miah <layekofficial63@gmail.com>
2025-11-09 23:50:13 +0600

    modify

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const booking = require("./routes/booking");
 // const cabin = require("./routes/cabin");
 // const guest = require("./routes/guest");
-const setting = require("./routes/setting");
+// const setting = require("./routes/setting");
 
 const app = express();
 
