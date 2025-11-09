69a434f6692368f941514a5dc037126ddbb50682
Layek Miah <layekofficial63@gmail.com>
2025-11-09 23:39:55 +0600

    modify

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-const routes = [
-  { path: "/api/bookings", router: booking },
-  { path: "/api/cabins", router: cabin },
-  { path: "/api/guests", router: guest },
-  { path: "/api/settings", router: setting },
-];
+// const routes = [
+//   { path: "/api/bookings", router: booking },
+//   { path: "/api/cabins", router: cabin },
+//   { path: "/api/guests", router: guest },
+//   { path: "/api/settings", router: setting },
+// ];
 
-routes.forEach(({ path, router }) => app.use(path, router));
+// routes.forEach(({ path, router }) => app.use(path, router));
 
 app.get("/", (req, res) => {
   res.send("The Wild Oasis Website Backend is working");
