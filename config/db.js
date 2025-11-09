451fbcde0a6801e7cb6da76002a298cacf2c09d3
Layek Miah <layekofficial63@gmail.com>
2025-11-09 23:21:32 +0600

    debug

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
 
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
-    isConnected = db.connection[0].readyState === 1;
+    isConnected = db.connections[0].readyState === 1;
     console.log("Database connect successfully");
   } catch (error) {
     throw new Error(error.message);
