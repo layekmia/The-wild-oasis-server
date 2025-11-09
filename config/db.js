423181427507bbb9eae56dbb76fd327835b64bb7
Layek Miah <layekofficial63@gmail.com>
2025-11-09 10:38:18 +0600

    modify db connect function

--- config/db.js
+++ config/db.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("connected to database");
-  });
+const MONGO_URI = process.env.MONGODB_URI;
+let cached = global.mongoose;
 
-  await mongoose.connect(process.env.MONGODB_URI);
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+const connectDB = async () => {
+  if (cached.conn) {
+    // Reuse existing connection
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
--- controller/cabin.controller.js
+++ controller/cabin.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Cabin = require("../model/cabin");
 
 exports.getCabins = async (req, res) => {
