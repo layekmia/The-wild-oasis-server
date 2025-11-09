c76e8cd5437106aa2ebba0a757fc58bf9795c4a9
Layek Miah <layekofficial63@gmail.com>
2025-11-10 00:46:54 +0600

    debug

--- routes/guest.js
+++ routes/guest.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { getGuest } = require("../controller/guest.controller");
 // const { getGuest } = require("../controller/guest.controller");
 
 const router = express.Router();
@@ -6,6 +7,6 @@ const router = express.Router();
 // router.post('/', createGuest);
 // router.get("/", getGuest);
 // router.patch('/:id/update', updateGuest);
-router.get('/', (req, res)=> {res.send("Hello")})
+router.get('/', getGuest)
 
 module.exports = router;
